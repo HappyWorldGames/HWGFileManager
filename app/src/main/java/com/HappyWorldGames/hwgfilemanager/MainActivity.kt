3cfa8c554aca1896b9da345c93feb865b44ca59e
HappyWorld <xeler.arsk@gmail.com>
2021-06-07 14:06:56 +0300

    Init commit

---
+++ app/src/main/java/com/HappyWorldGames/hwgfilemanager/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.HappyWorldGames.hwgfilemanager
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
